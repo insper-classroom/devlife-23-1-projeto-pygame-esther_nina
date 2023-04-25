34649a77f694969da535373fa1b3c52e2c232296
Esther <estherccr@al.insper.edu.com>
2023-04-25 16:01:44 -0300

    inicio tela fim

---
+++ code/classe_tela_fim.py
@@ -0,0 +1,48 @@
+import pygame
+from funcoes import *
+
+class TelaFim:
+    def __init__(self):
+        pygame.init()
+        
+        self.tamanho_tela = [500, 700]
+        self.window = pygame.display.set_mode(self.tamanho_tela)
+        self.imagem_inicio = pygame.image.load('assets/minerando.jpeg')
+        self.imagem_inicio = pygame.transform.scale(self.imagem_inicio, (700, 700))
+        
+        self.fonte = pygame.font.Font('assets/Emulogic-zrEw.ttf', 20)
+        self.jogo = Jogo()
+
+
+    def atualiza_tela(self):
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                return False
+            elif evento.type == pygame.K_r:
+                    self.jogo.game_loop()
+                    return False
+        return True
+
+    def desenha_tela_inicio(self):
+        self.window.blit(self.imagem_inicio, (- 87, 0))
+        pygame.draw.rect(self.window, AZUL_CLARINHO, (150, 500, 200, 100))
+        
+        game_over = self.fonte_titulo.render('A alma se perdeu...', self.fonte_titulo, ROXO_ESCURO)
+        self.window.blit(game_over, (46, 300))
+
+        restart = self.fonte.render('Pressione 'R' para recomeçar', self.fonte, ROXO_ESCURO)
+        self.window.blit(restart, (100, 650))
+
+        score =  self.fonte.render(f'Score: {self.score}', self.fonte, ROXO_ESCURO)
+        self.window.blit(score, (100, 540))
+
+        maior_score = self.fonte.render(f'Highest Score:: {self.maior_score}', self.fonte, ROXO_ESCURO)
+        self.window.blit(maior_score, (100, 600))
+        pygame.display.update()
+
+    def inicio_loop(self): # O loop para a tela de fim
+        inicio = True
+        while inicio:
+            inicio = self.atualiza_tela()
+            if inicio:
+                self.desenha_tela_inicio()
